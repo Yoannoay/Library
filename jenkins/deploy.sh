9a1ada8cf5bd61c4d70e1ec993061c4f799424a1
yoan <jimbob@gmail.com>
2021-11-29 19:11:05 +0000

    testing webhook

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -9,4 +9,4 @@ ssh jenkins@swarm-manager \
     DOCKER_HUB_CREDS_USR=$DOCKER_HUB_CREDS_USR \
     DATABASE_URI=$DATABASE_URI \
     CREATE_SCHEMA=true \
-    docker stack deploy --compose-file docker-compose.yaml todo-app
+    docker stack deploy --compose-file docker-compose.yaml library
