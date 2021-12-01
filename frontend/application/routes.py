ba8696765e1abf24d123b7bd78587c704fe63c0c
yoan <jimbob@gmail.com>
2021-12-01 00:38:31 +0000

    Tapping out for the night

--- frontend/application/routes.py
+++ frontend/application/routes.py
@@ -12,8 +12,8 @@ def home():
     app.logger.info(f"All_reviews: {all_reviews}")
     return render_template('index.html', title="Home", all_reviews=all_reviews["all reviews"])
 
-@app.route('/create/task', methods=['GET','POST'])
-def create_task():
+@app.route('/allbooks', methods=['GET'])
+def books_list():
     form = TaskForm()
 
     if request.method == "POST":
