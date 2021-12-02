26582b25a9155ed1b0dfef142757266bda9870d1
yoan <jimbob@gmail.com>
2021-12-02 00:41:07 +0000

    Commit

--- frontend/application/routes.py
+++ frontend/application/routes.py
@@ -29,24 +29,23 @@ def create_author():
     return render_template("create_author.html", title="Add Author", form=form)
 
 
-# @app.route('/create/book/<int:id>', methods=["GET", "POST"])
-# def create_book():
-#     form = CreatePlanetForm()
+@app.route('/create/book/<int:id>', methods=["GET", "POST"])
+def create_book(id):
+    form = NewBook()
 
-#     if request.method == "POST":
-#         response = requests.post(
-#             f"http://{backend}/create/planet",
-#             json={
-#                 "name": form.name.data,
-#                 "mass": form.mass.data,
-#                 "type": form.type.data,
-#                 "star_system": form.star_system.data
-#             }
-#         )
-#         app.logger.info(f"Response: {response.text}")
-#         return redirect(url_for("home"))
+    if request.method == "POST":
+        response = requests.post(
+            f"http://library_backend:5000/create/book/<int:id>",
+            json={
+                "name": form.name.data,
+                author_id=id 
+                
+            }
+        )
+        app.logger.info(f"Response: {response.text}")
+        return redirect(url_for("home"))
 
-#     return render_template("create_planet.html", title="Add Planet", form=form)
+    return render_template("create_book.html", title="New Book", form=form)
 
 
 # @app.route('/allbooks', methods=['GET'])
