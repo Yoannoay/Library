a5a15dc66f417fd224900f6e61810ea96af035e5
yoan <jimbob@gmail.com>
2021-12-02 11:00:42 +0000

    commit

--- backend/application/models.py
+++ backend/application/models.py
@@ -4,16 +4,14 @@ class Author(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(30), nullable=False, default="Unknown", unique = True)
     
-    books = db.relationship('Book', backref='author', cascade="delete",
-        passive_deletes=True)
+    books = db.relationship('Book', backref='author', cascade="delete")
     reviews =db.relationship('Review', backref='author')
 
 class Book(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(30), nullable= False)
 
-    reviews =db.relationship('Review', backref='book', cascade="all, delete",
-        passive_deletes=True)
+    reviews =db.relationship('Review', backref='book', cascade="delete")
 
     author_id = db.Column(db.Integer, db.ForeignKey('author.id'), nullable=True)
     
