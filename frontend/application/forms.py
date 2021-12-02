517345ef052a9be8aaf9f19539686cdc316a6684
yoan <jimbob@gmail.com>
2021-12-02 00:18:45 +0000

    Commit

--- frontend/application/forms.py
+++ frontend/application/forms.py
@@ -2,6 +2,6 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 
-class TaskForm(FlaskForm):
-    description = StringField("Task Description", validators=[DataRequired()])
-    submit = SubmitField("Add Task")
+class CreateAuthor(FlaskForm):
+    name = StringField("Author fullname", validators=[DataRequired()])
+    submit = SubmitField("Add Author")
