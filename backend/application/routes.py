c22889cabde77045f7980541b7e4bdf318f94d8f
yoan <jimbob@gmail.com>
2021-12-05 19:40:34 +0000

    Maybe fixed

--- backend/application/routes.py
+++ backend/application/routes.py
@@ -149,7 +149,7 @@ def update_book(id):
 @app.route('/update/review/<int:id>', methods=['PUT'])
 def update_review(id):
     package= request.json
-    id= package["review_id"]
+    # id= package["review_id"]
     review = Review.query.get(id)
     
     review.thoughts = package["thoughts"]
